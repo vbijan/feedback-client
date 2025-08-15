f35e2b973e4eb972449333dd48d3513725b0fad4
vbijan <bijan.gopali@gmail.com>
2025-08-15 17:18:31 -0500

    fixed case sensativity

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Index from "./pages/index";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
